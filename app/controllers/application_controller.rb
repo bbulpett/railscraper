eef017ee5a0572ddd0a8e8808f073e88d306e9e6
Barnabas Bulpett <bbulpett@charter.net>
2015-09-23 22:27:15 -0400

    add scraper method and basic view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,30 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Define the Headline object
+  class Headline
+  	def initialize(title, link)
+  		@title = title
+  		@link = link
+  	end
+  	attr_reader :title
+  	attr_reader :link
+  end
+
+  # Pull the site
   def scrape_chinadaily
-  	render text: 'scrape chinadaily data here'
+  	require 'open-uri'
+  	doc = Nokogiri::HTML(open("http://www.chinadaily.com.cn/entertainment/celebrities.html"))
+
+  	# Parse for headlines and create array
+  	headlines = doc.css('ul.chinanewslst>li')
+  	@headlinesArray = []
+  	headlines.each do |h|
+  		title = h.css('h2.titlebox>a').text
+  		link = h.css('h2.titlebox>a')[0]['href']
+  		@headlinesArray << Headline.new(title, link)
+  	end
+
+  	render template: 'scrape_chinadaily'
   end
 end
